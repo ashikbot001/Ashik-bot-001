4267673b3f85ee79d6641000d91b76cf50054d86
ashikbot001 <bossagor745@gmail.com>
2024-10-09 21:20:26 +0600

    Update Joy27.js

--- scripts/commands/Joy27.js
+++ scripts/commands/Joy27.js
@@ -16,13 +16,13 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("😥")==0 || body.indexOf("😢")==0 || body.indexOf("😮‍💨")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😮‍")==0) {
 		var msg = {
-				body: "পৃথিবীতে প্রত্যেকটা মানুষের\nহৃদয়ে ব্যথা আছে\nশুধু প্রকাশ করার ধরন টা আলাদা….!.!\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "পৃথিবীতে প্রত্যেকটা মানুষের\nহৃদয়ে ব্যথা আছে\nশুধু প্রকাশ করার ধরন টা আলাদা….!.!\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joy56.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
