69dbb31c89a974bfcc32ab3b7b561ba9c0d044f7
ashikbot001 <bossagor745@gmail.com>
2024-10-09 21:15:09 +0600

    Update Joy22.js

--- scripts/commands/Joy22.js
+++ scripts/commands/Joy22.js
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf("😦")==0 || body.indexOf("😩")==0 || body.indexOf("😫")==0 || body.indexOf("😢")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😢")==0) {
 		var msg = {
-				body: "মানুষ কথা দেয় শুধু মাত্র মুহুর্ত সুন্দর করার জন্য🙂\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "মানুষ কথা দেয় শুধু মাত্র মুহুর্ত সুন্দর করার জন্য🙂\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/K.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
