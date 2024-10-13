3f1f4d13248110f9a6430f6cd87c6d342f4297f6
ashikbot001 <bossagor745@gmail.com>
2024-10-14 01:18:38 +0600

    Update Joy6.js

--- scripts/commands/Joy6.js
+++ scripts/commands/Joy6.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf("💋")==0 || body.indexOf("💏")==0 || body.indexOf("👩‍❤️‍💋‍👨")==0 || body.indexOf("💑")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "___💜🔐🌈 ___ 𝗧𝗵𝗶𝘀 𝗮𝗯𝗼𝘂𝘁 𝗹𝗶𝗻𝗲!! 🙂🧡✨🐰!<'— পূর্ণতা পাক পৃথিবীর সকল ভালোবাসা\nসুন্দর হোক সম্পর্কের শেষটা -!!-) 😊🖤🌻\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joy121.mp4`)
