b88aae08d2e7ef7d28c0eccc63902db6ce5c2112
ashikbot001 <bossagor745@gmail.com>
2024-10-14 01:12:43 +0600

    Update Joy27.js

--- scripts/commands/Joy27.js
+++ scripts/commands/Joy27.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😮‍")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "পৃথিবীতে প্রত্যেকটা মানুষের\nহৃদয়ে ব্যথা আছে\nশুধু প্রকাশ করার ধরন টা আলাদা….!.!\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joy56.mp3`)
