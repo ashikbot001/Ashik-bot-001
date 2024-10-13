faffa46eeed94b1e84b61a0438e27758eea11eee
ashikbot001 <bossagor745@gmail.com>
2024-10-14 01:10:51 +0600

    Update Joy23.js

--- scripts/commands/Joy23.js
+++ scripts/commands/Joy23.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😬")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "যেখানে স্বপ্নয় আমাদের\nএক করতে পারে না\nসেখানে বাস্তবতা তাে নির্মম।🙂\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joytt.mp3`)
