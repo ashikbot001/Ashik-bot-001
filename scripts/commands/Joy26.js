4f9ea22b3da5d187b66fdb0b173ee821fd9871eb
ashikbot001 <bossagor745@gmail.com>
2024-10-14 01:12:06 +0600

    Update Joy26.js

--- scripts/commands/Joy26.js
+++ scripts/commands/Joy26.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("n")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "চিৎকার করে কখনও নিজেকে\nনির্দোষ প্রমাণ করা যায়না!\nমাঝে মাঝে চুপ থাকতে হয় !\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joy55.mp3`)
